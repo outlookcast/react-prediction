50a7014f038616b3d55735a68b7083bcb2dead1f
Vinicius Sampaio <vsampaio@ice.ufjf.br>
2019-08-03 22:49:23 -0300

    Updates

---
+++ precache-manifest.3b3fee00e6469db3d20899f9adb9e446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6821176be13fd1e505df6935182387",
+    "url": "/react-prediction/index.html"
+  },
+  {
+    "revision": "3c262bf0370685a58553",
+    "url": "/react-prediction/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "afdf597440c39c343b8a",
+    "url": "/react-prediction/static/js/2.9376967e.chunk.js"
+  },
+  {
+    "revision": "3c262bf0370685a58553",
+    "url": "/react-prediction/static/js/main.f83101df.chunk.js"
+  },
+  {
+    "revision": "08c20af1727d10dfa4c5",
+    "url": "/react-prediction/static/js/runtime~main.58dfb16e.js"
+  }
+]);
